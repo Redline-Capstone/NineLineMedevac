a8a7813724c5d79eee62072babe4667765694a77
SIDNEY <73758696+hallsidney@users.noreply.github.com>
2021-04-20 16:54:16 -0500

    Fixed Table Border
    Rich and I fixed the border after merge conflict

--- front_end/src/NineLineCreator.js
+++ front_end/src/NineLineCreator.js
@@ -141,18 +141,7 @@ class NineLineCreator extends Component {
                                 <td>Call Sign</td>
                                 <td>
                                     <input class="form-control" required defaultValue="Line 2" value={this.state.callSign} name="callSign"></input><br />
-
                                 </td>
-                                    <td>
-                                        <input class="form-control" required defaultValue="Line 1" value={this.state.location} name="location"></input>
-                                    </td>
-                                </tr>
-                                <tr class="table-required">
-                                    <th scope="row">2</th>
-                                    <td>Call Sign</td>
-                                    <td>
-                                        <input class="form-control" required defaultValue="Line 2" value={this.state.callSign} name="callSign"></input><br />
-                                    </td>
                                 </tr>
                                 <tr class="table-required">
                                     <th scope="row">3</th>
