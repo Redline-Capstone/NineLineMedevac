e3500aa2ea9828f829a7a0990e31cd7663996051
SIDNEY <73758696+hallsidney@users.noreply.github.com>
2021-04-21 14:20:36 -0500

    Update Summary.js

--- front_end/src/Summary.js
+++ front_end/src/Summary.js
@@ -3,12 +3,51 @@ import { Component } from 'react'
 export default class Summary extends Component{
     constructor(props){
         super(props)
-        this.state = {};
+        this.state = {
+            currentSummary:""
+
+        };
+        
+    }
+
+    updateCurrentSummary(choice){
+        this.setState({currentSummary:choice})
     }
 
     render(){
+        let temp = this.props.requests
+        temp.sort((a,b) =>{
+            let fa = a.responder.toLowerCase(),
+                fb = b.responder.toLowerCase();
+        
+            if (fa < fb) {
+                return -1;
+            }
+            if (fa > fb) {
+                return 1;
+            }
+            return 0;
+        })
+        return(
         <div>
             <h1>SUMMARY</h1>
+            <Select options={this.props.responderList.concat("All")} onChange={(choice)=>{this.updateCurrentSummary(choice)}}></Select>
+            {this.state.currentSummary?
+            this.props.temp.map(
+                    (request, index) => {
+                        if(this.state.currentSummary != 'All'){
+                            if (request.responder === "") {
+                                return (
+                                    <Request
+                                    key={index}
+                                    requestObject={request}
+                                    setCurrentMissionAssignment={props.setCurrentMissionAssignment}
+                                    completeClick={props.completeClick}
+                                    />
+                                    )
+                                }
+                            }}
+                        ):""}
         </div>
-    }
+        );}
 }
