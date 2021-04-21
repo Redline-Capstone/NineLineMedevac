ea16a36d2e9578864cee304a7e59362038c5e7aa
SIDNEY <73758696+hallsidney@users.noreply.github.com>
2021-04-21 13:08:49 -0500

    Create Summary.js

---
+++ front_end/src/Summary.js
@@ -0,0 +1,14 @@
+import { Component } from 'react'
+
+export default class Summary extends Component{
+    constructor(props){
+        super(props)
+        this.state = {};
+    }
+
+    render(){
+        <div>
+            <h1>SUMMARY</h1>
+        </div>
+    }
+}
