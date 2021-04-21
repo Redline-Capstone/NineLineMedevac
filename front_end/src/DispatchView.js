6e5b4297943a54b0ed061778a100e19061fa742e
Schoony415 <joshua.schoonover@austincc.edu>
2021-04-21 16:03:57 -0500

    fixed a deleted }

--- front_end/src/DispatchView.js
+++ front_end/src/DispatchView.js
@@ -56,7 +56,7 @@ const DispatchView = (props) => {
                                 />
                                 )
                             }
-                        )}
+                        })}
                     </tbody>
                 </table>
                 <br />
