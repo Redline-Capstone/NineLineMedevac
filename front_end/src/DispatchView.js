3ab505a31a78cecd6c16efaa9769002131279fea
relerick <78380734+relerick@users.noreply.github.com>
2021-04-21 16:13:53 -0500

    rich
    curly bracket missing

--- front_end/src/DispatchView.js
+++ front_end/src/DispatchView.js
@@ -56,6 +56,7 @@ const DispatchView = (props) => {
                                 />
                                 )
                             }
+                        }
                         )}
                 </tbody>
             </table>
