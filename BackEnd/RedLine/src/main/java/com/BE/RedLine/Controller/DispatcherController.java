4fc8cabd2a5d6435cdee2beac9f362878987cdfc
Schoony415 <joshua.schoonover@austincc.edu>
2021-04-22 14:39:25 -0500

    updated back end
    now matches heroku

--- BackEnd/RedLine/src/main/java/com/BE/RedLine/Controller/DispatcherController.java
+++ BackEnd/RedLine/src/main/java/com/BE/RedLine/Controller/DispatcherController.java
@@ -1,7 +1,9 @@
 package com.BE.RedLine.Controller;
 
-import com.BE.RedLine.DAO.*;
+import com.BE.RedLine.DAO.RequestRepo;
+import com.BE.RedLine.DAO.ResponderRepo;
 import com.BE.RedLine.Model.Request;
+import com.BE.RedLine.Model.Responder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -11,24 +13,33 @@ import java.util.Optional;
 //@RequestMapping("")
 public class DispatcherController {
     final RequestRepo requestRepository;
+    final ResponderRepo responderRepository;
 
-    public DispatcherController(RequestRepo requestRepository) {
+    public DispatcherController(RequestRepo requestRepository,
+                                ResponderRepo responderRepository) {
         this.requestRepository = requestRepository;
+        this.responderRepository = responderRepository;
     }
 
     //get all
-    @GetMapping("/requests")
+//    @GetMapping("/requests")
+    @CrossOrigin
+    @RequestMapping(method = RequestMethod.GET, path="/requests")
     public Iterable<Request> getall(){
         return requestRepository.findAll();
     }
     //get one
-    @GetMapping("/requests/{id}")
+//    @GetMapping("/requests/{id}")
+    @CrossOrigin
+    @RequestMapping(method=RequestMethod.GET, path="/requests/{id}")
     public Optional<Request> getone(@PathVariable long id){
         return requestRepository.findById(id);
     }
     //patch one
-    @PatchMapping("/requests/{id}") //takes a key value pair for each paramater to change
-    public Request assign(@PathVariable long id, @RequestBody Map<String,String> input){
+//    @PatchMapping("/requests/{id}") //takes a key value pair for each paramater to change
+    @CrossOrigin
+    @RequestMapping(method=RequestMethod.PATCH, path= "/requests/{id}")
+    public Request patch(@PathVariable long id, @RequestBody Map<String,String> input){
         Request request = requestRepository.findById(id).get();
         if(request == null){return null;}
         //for each key value pair
@@ -36,10 +47,36 @@ public class DispatcherController {
             //set the associated property
             switch (key.toLowerCase()){
                 case "responder": request.setResponder(input.get(key)); break;
-                case "completed": request.setCompleted(true);break;
+                    //request = this.jointables(input.get(key) ,request); break;
+                case "completed": request.setCompleted(Boolean.parseBoolean((input.get(key)))); break;
+                //updating the nineline
+                case "line6" :
+                case "security" : request.setSecurity(input.get(key)); break;
+                case "line7" :
+                case "hlzmarking" : request.setHlzMarking(input.get(key)); break;
+                case "line8" :
+                case "nationality" : request.setNationality(input.get(key)); break;
+                case "line9" :
+                case "nbc" : request.setNbc(input.get(key)); break;
             }
         }
 
         return requestRepository.save(request);
     }
+
+//    //handler for setting responder to request
+//    public Request jointables(String responder, Request request){
+//        //look up responder
+//        Optional<Responder> tempResponder = responderRepository.findByName(responder);
+//        if(tempResponder == null){
+//            return null;
+//        }
+//        //set responder to request
+//        request.setResponder(tempResponder.get());
+//        //save request //this happens in patch function
+//        //return requestRepository.save(request);
+//        return request;
+//    }
+
+
 }
--- BackEnd/RedLine/src/main/java/com/BE/RedLine/Model/Request.java
+++ BackEnd/RedLine/src/main/java/com/BE/RedLine/Model/Request.java
@@ -1,60 +1,41 @@
 package com.BE.RedLine.Model;
 
 
-import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name="Request")
-public class Request extends NineLine{
+@AllArgsConstructor @NoArgsConstructor
+@Getter @Setter
+public class Request extends NineLine {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private boolean completed = false;
+
+
+//    @ManyToOne(fetch=FetchType.LAZY)//, cascade = CascadeType.ALL) //when you delete with cascade it also kills my spaceship
+//    @JoinColumn(name="requestList")
+//    @JsonBackReference
     private String responder = "";
 
-    @JsonCreator
-    public Request(String location, String callSign, String patientUrgency, String specialEquipment, String patientType, String security, String hlzMarking, String nationality, String nbc, long id, boolean completed, String responder) {
-        super(location, callSign, patientUrgency, specialEquipment, patientType, security, hlzMarking, nationality, nbc);
-        this.id = id;
-        this.completed = completed;
-        this.responder = responder;
-    }
 
-    public Request(String location, String callSign, String patientUrgency, String specialEquipment, String patientType) {
+    public Request( //5line
+             String location,
+             String callSign,
+             String patientUrgency,
+             String specialEquipment,
+             String patientType){
         super(location, callSign, patientUrgency, specialEquipment, patientType);
     }
-
-    public Request( NineLine nineLine){
-        super(nineLine.getLocation(), nineLine.getCallSign(), nineLine.getPatientUrgency(),nineLine.getSpecialEquipment(), nineLine.getPatientType(), nineLine.getSecurity(), nineLine.getHlzMarking(), nineLine.getNationality(), nineLine.getNbc());
+    public Request(NineLine input){ //with super class
+        super(input.getLocation(),input.getCallSign(),input.getPatientUrgency(),input.getSpecialEquipment(),input.getPatientType(),input.getSecurity(),input.getHlzMarking(),input.getNationality(),input.getNbc());
     }
 
-    public Request(){
-        super();
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public boolean isCompleted() {
-        return completed;
-    }
-
-    public void setCompleted(boolean completed) {
-        this.completed = completed;
-    }
-
-    public String getResponder() {
-        return responder;
-    }
-
-    public void setResponder(String responder) {
-        this.responder = responder;
-    }
 }
