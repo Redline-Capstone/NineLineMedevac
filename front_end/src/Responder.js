ed053b2f1f920e5ab995d33b3eca5774d89cc2f3
Schoony415 <joshua.schoonover@austincc.edu>
2021-04-22 13:45:34 -0500

    responder js
    === on line 12
    else return '' in the requests.map

--- front_end/src/Responder.js
+++ front_end/src/Responder.js
@@ -9,7 +9,7 @@ const Responder = (props) => {
 
         <div >
             <br />
-            <h1>  {props.selectedResponderView} { (props.selectedResponderView == "Responder Table") ? "" : " Missions"} </h1>  
+            <h1>  {props.selectedResponderView} { (props.selectedResponderView === "Responder Table") ? "" : " Missions"} </h1>  
         
             <Select className='dropDown' options={ props.responderList }  values={[{value:props.current, label: props.current}]} onChange={(choice) => props.setCurrentSelection(choice) }></Select>
            
@@ -44,7 +44,7 @@ const Responder = (props) => {
                         return (<Request key={"" + request.id + i + request.responder + request.completed} requestObject={request} completeClick={props.completeClick} 
                         />
                         )
-                    }
+                    } else return ""
                 }
                 )}
         </tbody>
